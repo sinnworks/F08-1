e850d3fd6e5f2f9a7b2240939c28162777e23aba
sinnworks <139361573+sinnworks@users.noreply.github.com>
2023-09-20 13:47:32 +0800

    寫在VM

--- F08-1/Models/ViewModels/ContactUsVm.cs
+++ F08-1/Models/ViewModels/ContactUsVm.cs
@@ -8,14 +8,34 @@ using System.Web;
 namespace F08_1.Models.ViewModels
 {
     public class ContactUsVm
-    {        
+    {
         public int Id { get; set; }
 
+        [Display(Name = "姓名")]
         [Required]
+        [StringLength(50)]
         public string Name { get; set; }
-        public HttpPostedFileBase File { get; set; }
 
-        public string FileName { get; set; }
+        [Display(Name = "性別")]
+        [Required]
+        [StringLength(10)]
+        public string Gender { get; set; }
+
+        [Display(Name = "留言內容")]
+        [Required]
+        [StringLength(1000)]
+        public string Suggestion { get; set; }
+
+        public byte[] Image { get; set; }
+
+        [Display(Name = "手機號碼")]
+        [Required]
+        [StringLength(10)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "上傳檔案")]
+        [Required]
+        public HttpPostedFileBase File { get; set; }
     }
 
     public class P010A02VM
